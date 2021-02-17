0431ebfc25547a07121dfcbed9014d2f8eec9dec
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-02-17 17:50:41 -0600

    Tradio & checkbox WIP

---
+++ src/components/TCheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    v-model="localValue"
+    type="checkbox"
+    :class="variantConfiguration.class"
+  >
+</template>
+
+<script lang="ts">
+import { WithVariantProps, TCheckboxTheme } from '@variantjs/core';
+import { PropType } from 'vue';
+import defineVariantComponent from '../utils/defineVariantComponent';
+
+export type TCheckboxValue = string | number | undefined | null;
+
+export type TCheckboxProps = WithVariantProps<{
+  modelValue: TCheckboxValue
+}>;
+
+export default defineVariantComponent('TCheckbox', {
+  props: {
+    modelValue: {
+      type: [String, Number] as PropType<TCheckboxValue>,
+      default: undefined,
+    },
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:modelValue': (_: TCheckboxValue) => true,
+  },
+  computed: {
+    localValue: {
+      get(): TCheckboxValue {
+        return this.modelValue;
+      },
+      set(value: TCheckboxValue) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+}, TCheckboxTheme);
+
+</script>
---
+++ src/components/TRadio.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    v-model="localValue"
+    type="radio"
+    :class="variantConfiguration.class"
+  >
+</template>
+
+<script lang="ts">
+import { WithVariantProps, TRadioTheme } from '@variantjs/core';
+import { PropType } from 'vue';
+import defineVariantComponent from '../utils/defineVariantComponent';
+
+export type TRadioValue = string | number | undefined | null;
+
+export type TRadioProps = WithVariantProps<{
+  modelValue: TRadioValue
+}>;
+
+export default defineVariantComponent('TRadio', {
+  props: {
+    modelValue: {
+      type: [String, Number] as PropType<TRadioValue>,
+      default: undefined,
+    },
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:modelValue': (_: TRadioValue) => true,
+  },
+  computed: {
+    localValue: {
+      get(): TRadioValue {
+        return this.modelValue;
+      },
+      set(value: TRadioValue) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+}, TRadioTheme);
+
+</script>
