78b56a266a1448515689b5cf2e8e9fc2f38418d5
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-17 17:53:02 -0500

    development main and app.vue files

--- src/App.vue
+++ src/App.vue
@@ -1,205 +1,64 @@
 <template>
-  <div style="padding: 15px">
-    <!-- <t-input
-      :id="id"
-      fixed-classes="block w-full px-3 py-2  transition duration-100 ease-in-out border rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
-      classes="text-black placeholder-gray-400 bg-white border-gray-300"
-      :type="type"
-      :variants="variants"
-      :variant="variant"
-    />-->
+  <div class="pb-36">
+    <div class="max-w-md mx-auto my-10">
+      <div class="grid grid-cols-1 gap-6">
+        <t-input placeholder="My placeholder" />
+        <t-textarea placeholder="My placeholder" />
+        <t-select :options="options" />
+        <label class="flex items-center">
+          <t-checkbox checked />
+          <span class="ml-2">Check me</span>
+        </label>
+        <div>
+          <label class="flex items-center">
+            <t-radio
+              name="radio"
+              checked
+            />
 
-    <t-input
-      v-model="test"
-      :variant="undefined"
-    />
-    <t-input
-      v-model="test"
-      variant="error"
-    />
+            <span class="ml-2">Select this</span>
+          </label>
+          <label class="flex items-center">
+            <t-radio name="radio" />
 
-    <t-input
-      id="hola"
-      v-model="id"
-      type="text"
-      data-test="test"
-      :variant="variant"
-      classes="sadgadsgs xxs"
-    />
+            <span class="ml-2">Or select this</span>
+          </label>
+        </div>
 
-    <!--<t-input
-      v-model="id"
-      type="text"
-      :variant="variant"
-    />-->
-
-    <t-select
-      v-model="variant"
-      :options="['test2', 'test', 'error', 'ther']"
-    />
-
-    <!-- {{ variant }}
-
-    {{ id }}
-
-    {{ type }}
-
-    <select v-model="variant">
-      <option value="success">
-        Success
-      </option>
-      <option value="error">
-        Error
-      </option>
-    </select>-->
-
-    <t-checkbox
-      v-model="test2"
-      name="test"
-      value="1"
-      variant="test"
-      :variants="{
-        test: {
-          classes: 'text-red-500 border-red-500'
-        }
-      }"
-    />
-    <t-checkbox
-      v-model="test2"
-      name="test"
-      value="2"
-      classes="text-blue-500 border-blue-500"
-    />
-    <t-checkbox
-      v-model="test2"
-      name="test"
-      value="3"
-    />
-
-    <!-- <t-wrapped-radio
-      id="test2-1"
-      v-model="test"
-      name="test2"
-      value="1"
-      label="1"
-    />
-    <t-wrapped-radio
-      id="test2-2"
-      v-model="test"
-      name="test2"
-      :value="{text: 1}"
-      label="2"
-    />
-    <t-wrapped-radio
-      id="test2-3``"
-      v-model="test"
-      name="test2"
-      value="3"
-      label="3"
-    /> -->
-
-    <!--<div action="">
-      <t-wrapped-radio
-        name="test3"
-        :value="true"
-      >
-        gola 1
-      </t-wrapped-radio>
-      <t-wrapped-radio
-        checked
-        name="test3"
-        :value="{text: 1}"
-      >
-        Hoal 2
-      </t-wrapped-radio>
-
-      <t-wrapped-radio
-        :fixed-classes="{
-          wrapper: 'text-blue-500'
-        }"
-        name="test3"
-        value="3"
-      >
-        Hola 3
-      </t-wrapped-radio>
+        <!-- <t-button>My button</t-button> -->
+      </div>
     </div>
-
-    <t-wrapped-radio
-      name="test3"
-      :value="[1,2,3]"
-    >
-      Hola 4
-    </t-wrapped-radio>
-
-    <t-wrapped-radio
-      name="test3"
-      :value="() => {console.log('')}"
-    >
-      Hola 5
-    </t-wrapped-radio>
-
-    <p>{{ test }}</p>
-
-    <t-select
-      v-model="type"
-      :options="['number', 'text']"
-    />
-
-    <t-select
-      v-model="number"
-      :options="[1, 2, 3]"
-    />-->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 import TInput from './components/TInput.vue';
 import TSelect from './components/TSelect.vue';
-// import TRadio from './components/TRadio.vue';
-// import TWrappedRadio from './components/TWrappedRadio.vue';
+import TRadio from './components/TRadio.vue';
 import TCheckbox from './components/TCheckbox.vue';
-import './assets/tailwind.css';
+import TTextarea from './components/TTextarea.vue';
+// import TButton from './t-button';
 
 export default defineComponent({
   name: 'App',
   components: {
     TInput,
-    TSelect,
-    // TRadio,
-    // TWrappedRadio,
+    TTextarea,
+    TRadio,
     TCheckbox,
+    TSelect,
+    // TButton,
   },
   data() {
     return {
-      test: '3',
-      test2: [],
-      number: 2,
-      variants: {
-        error: {
-          id: 'error',
-          type: 'number',
-          title: 'Hola',
-          placeholder: 'error',
-          classes: 'text-red-500 placeholder-red-300 bg-red-100 border-gray-500',
-        },
-        ther: {
-          classes: undefined,
-          fixedClasses: undefined,
-        },
-      },
-      id: '',
-      variant: 'success',
-      type: 'text',
+      options: [
+        'Option A',
+        'Option B',
+        'Option C',
+      ],
     };
-  },
-  methods: {
-    onChange() {
-      console.log('Change');
-    },
-    onFocus() {
-      console.log('docus');
-    },
   },
 
 });
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,13 @@
+import './assets/tailwind.css';
 import { createApp } from 'vue';
-
-import variantJsPlugin, { VariantJSConfiguration } from '.';
+import { VariantJSConfiguration } from './types';
+import variantJsPlugin from '.';
 
 import App from './App.vue';
 
 const app = createApp(App);
 
-const configuration: VariantJSConfiguration = {
-  // TCheckbox: {
-  //   type: 'checkbox',
-  // },
-  // TSelect: {
-  //   autofocus: true,
-  //   'data-something': 'somethign',
-  // },
-  // TInput: {
-  //   'data-something': 'somethign',
-  //   'aria-label': 'gsdgds',
-  //   maxlength: 10,
-  //   placeholder: 'type something',
-  //   classes: 'block w-full pl-3 pr-10 py-2 text-black placeholder-gray-400 transition duration-100 ease-in-out bg-white border border-gray-300 rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-  //   variants: {
-  //     error: {
-  //       classes: 'text-red-500 bg-red-200',
-  //       maxlength: undefined,
-  //       placeholder: 'Something wrong happen',
-  //       'data-something': undefined,
-  //       'aria-label': 'other',
-  //     },
-  //   },
-  // },
-};
+const configuration: VariantJSConfiguration = {};
 
 app.use(variantJsPlugin, configuration);
 
