72dd258fb7921b75eed09e38ca599a8bdf78a294
alfonsobries <alfonsobries@gmail.com>
2021-08-28 13:13:55 -0500

    Report test coverage with html

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     "!**/node_modules/**",
     "!**/*.d.ts"
   ],
+  coverageReporters: ["json", "html"],
   testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
   coveragePathIgnorePatterns: [
     "/node_modules/",
