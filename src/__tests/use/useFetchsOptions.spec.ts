fc2dd99b7af2b2bd0ddbf66af997e77eefd53598
alfonsobries <alfonsobries@gmail.com>
2021-08-30 17:06:34 -0500

    Test options that come from a the fetch function

--- src/__tests/use/useFetchsOptions.spec.ts
+++ src/__tests/use/useFetchsOptions.spec.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 import { FetchOptionsFn, MinimumInputLengthTextProp } from '../../types';
 import useFetchsOptions from '../../use/useFetchsOptions';
 import { useSetup } from './useSetup';
@@ -152,6 +152,206 @@ describe('useFetchsOptions', () => {
           expect(normalizedOptions.value).toEqual([
             { raw: 'Option B', text: 'Option B', value: 'Option B' },
             { raw: 'Option B2', text: 'Option B2', value: 'Option B2' },
+          ]);
+        });
+      });
+    });
+  });
+
+  describe('with fetch function and no initial options', () => {
+    beforeEach(() => {
+      options.value = [];
+      fetchFn.value = () => new Promise((resolve) => resolve({
+        results: ['A', 'B'],
+      }));
+      jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    it('returns normalized options', () => {
+      useSetup(async () => {
+        const { normalizedOptions, fetchOptions } = useFetchsOptions(
+          options,
+          textAttribute,
+          valueAttribute,
+          normalize,
+          searchQuery,
+          fetchFn,
+          fetchDelay,
+          fetchMinimumInputLength,
+          fetchMinimumInputLengthText,
+        );
+
+        expect(normalizedOptions.value).toEqual([]);
+
+        fetchOptions();
+
+        await nextTick();
+
+        expect(normalizedOptions.value).toEqual([
+          { raw: 'A', text: 'A', value: 'A' },
+          { raw: 'B', text: 'B', value: 'B' },
+        ]);
+      });
+    });
+
+    it('handles undefined options', () => {
+      useSetup(async () => {
+        fetchFn.value = () => new Promise((resolve) => resolve({
+          results: undefined!,
+        }));
+
+        const { fetchOptions, normalizedOptions } = useFetchsOptions(
+          options,
+          textAttribute,
+          valueAttribute,
+          normalize,
+          searchQuery,
+          fetchFn,
+          fetchDelay,
+          fetchMinimumInputLength,
+          fetchMinimumInputLengthText,
+        );
+
+        expect(normalizedOptions.value).toEqual([]);
+
+        fetchOptions();
+
+        await nextTick();
+
+        expect(normalizedOptions.value).toEqual([
+          { raw: 'A', text: 'A', value: 'A' },
+          { raw: 'B', text: 'B', value: 'B' },
+        ]);
+      });
+    });
+
+    it('accepts a custom `textAttribute`', () => {
+      useSetup(async () => {
+        fetchFn.value = () => new Promise((resolve) => resolve({
+          results: [
+            { label: 'Letter A', value: 'A' },
+            { label: 'Letter B', value: 'B' },
+          ],
+        }));
+
+        const { normalizedOptions, fetchOptions } = useFetchsOptions(
+          options,
+          ref('label'),
+          valueAttribute,
+          normalize,
+          searchQuery,
+          fetchFn,
+          fetchDelay,
+          fetchMinimumInputLength,
+          fetchMinimumInputLengthText,
+        );
+
+        expect(normalizedOptions.value).toEqual([]);
+
+        fetchOptions();
+
+        await nextTick();
+
+        expect(normalizedOptions.value).toEqual([
+          { raw: { label: 'Letter A', value: 'A' }, text: 'Letter A', value: 'A' },
+          { raw: { label: 'Letter B', value: 'B' }, text: 'Letter B', value: 'B' },
+        ]);
+      });
+    });
+
+    it('accepts a custom `valueAttribute`', () => {
+      useSetup(async () => {
+        fetchFn.value = () => new Promise((resolve) => resolve({
+          results: [
+            { text: 'A', identifier: 'a' },
+            { text: 'B', identifier: 'b' },
+          ],
+        }));
+
+        const { normalizedOptions, fetchOptions } = useFetchsOptions(
+          options,
+          textAttribute,
+          ref('identifier'),
+          normalize,
+          searchQuery,
+          fetchFn,
+          fetchDelay,
+          fetchMinimumInputLength,
+          fetchMinimumInputLengthText,
+        );
+
+        expect(normalizedOptions.value).toEqual([]);
+
+        fetchOptions();
+
+        await nextTick();
+
+        expect(normalizedOptions.value).toEqual([
+          { raw: { text: 'A', identifier: 'a' }, text: 'A', value: 'a' },
+          { raw: { text: 'B', identifier: 'b' }, text: 'B', value: 'b' },
+        ]);
+      });
+    });
+
+    it('doesnt normalize the options if normalize is `false`', () => {
+      useSetup(async () => {
+        const { normalizedOptions, fetchOptions } = useFetchsOptions(
+          options,
+          textAttribute,
+          valueAttribute,
+          ref(false),
+          searchQuery,
+          fetchFn,
+          fetchDelay,
+          fetchMinimumInputLength,
+          fetchMinimumInputLengthText,
+        );
+
+        expect(normalizedOptions.value).toEqual([]);
+
+        fetchOptions();
+
+        await nextTick();
+
+        expect(normalizedOptions.value).toEqual(options.value);
+      });
+    });
+
+    describe('with search query', () => {
+      it('filters by the search query', () => {
+        fetchFn.value = (query?: string) => new Promise((resolve) => resolve({
+          results: [
+            { value: query, text: query },
+          ],
+        }));
+
+        searchQuery.value = 'test ';
+
+        useSetup(async () => {
+          const { normalizedOptions, fetchOptions } = useFetchsOptions(
+            options,
+            textAttribute,
+            valueAttribute,
+            normalize,
+            searchQuery,
+            fetchFn,
+            fetchDelay,
+            fetchMinimumInputLength,
+            fetchMinimumInputLengthText,
+          );
+
+          expect(normalizedOptions.value).toEqual([]);
+
+          fetchOptions();
+
+          await nextTick();
+
+          expect(normalizedOptions.value).toEqual([
+            { raw: 'test', text: 'test', value: 'test' },
           ]);
         });
       });
