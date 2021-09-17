9600ffaae14713dfe5eef83d7609ba779650fe38
alfonsobries <alfonsobries@gmail.com>
2021-09-17 18:29:14 -0500

    Release try 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'variantjs-vue',
+      fileName: (format) => `vue.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
+
