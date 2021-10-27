a9810c4e4e668a4c6a16bd8ec6ce5e2b2a871de0
alfonsobries <alfonsobries@gmail.com>
2021-10-27 15:11:56 -0500

    rollback es build

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     minify: true,
     sourcemap: true,
     lib: {
-      formats: ['cjs', 'umd'],
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'VariantJS',
       fileName: (format) => `index.${format}.js`
